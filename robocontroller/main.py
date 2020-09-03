bd1a619d520265d5745bd298c5a08e094289bace
Simon Christensen <c3lphie@protonmail.com>
2020-09-03 21:39:41 +0200

    Prepared python barebones python files

---
+++ robocontroller/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env pybricks-micropython
+
+
+def run():
+    return
+
+
+if __name__ == "__main__":
+    run()
---
+++ robocontroller/modules/navigation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env pybricks-micropython
+
+# ==========================================================
+# This is the module used to make the robot follow the line,
+# and other smart navigation related functions.
+# ==========================================================
+
+
+def seek_line():
+    return
+
+
+def follow_line():
+    return
