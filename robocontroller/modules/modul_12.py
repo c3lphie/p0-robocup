d3cbc001d9e29fee00b2205620c5b04a7ef76d7e
holmdane <57073689+holmdane@users.noreply.github.com>
2020-09-07 14:32:19 +0200

    Update modul_12.py

--- robocontroller/modules/modul_12.py
+++ robocontroller/modules/modul_12.py
@@ -1,5 +1,19 @@
-#drej 45 grader mod højre
-#kør frem x antal sekunder
-#drej 90+ grader
-#kør frem til linje findes
-#Følg linje til markør
+import navigation
+
+# drejer 45 grader mod højre
+robot.turn(45)
+
+# køre 15 cm frem
+robot.distance(150) 
+
+# drejer 90+ grader mod venstre
+robot.turn(-100)
+
+# køre frem til grå linje findes og følger den frem til markøren
+ 
+while : True
+    if line_sensor.reflection() <= white + 5:
+        robot.stop()
+        navigation.follow_line()
+        return : True
+return False
