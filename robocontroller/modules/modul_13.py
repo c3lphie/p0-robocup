366dd73ab6bf5d5d76d1410768c27d34e234db49
Kenneth Flye <milleflye@gmail.com>
2020-09-07 13:45:12 +0200

    Update modul_13.py

--- robocontroller/modules/modul_13.py
+++ robocontroller/modules/modul_13.py
@@ -1,6 +1,22 @@
-#kør frem til hvid rammes
-#mål afstand til væg
-#del afstand med to
-#kør frem til afstands måler viser forrige svar
-#stop
-#Afspil we are the champions
+import navigation
+# kør frem til hvid rammes
+while line_sensor.reflection() not <= WHITE - 5:
+    robot.drive(500)
+
+# mål afstand til væg
+Afstand = distance()
+
+# del afstand med to
+Afstand_stop = Afstand / 2
+
+# kør frem til afstands måler viser forrige svar
+while True
+  Distance_measured = distance()
+   if Distance_measured == Afstand_stop:
+        robot.stop()
+    else:
+        follow_line  # Hvid
+
+
+# stop
+# Afspil we are the champions
