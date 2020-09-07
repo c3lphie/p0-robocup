7e4ea40765a852ec243410e327a22d80a1671ec0
Kenneth Flye <milleflye@gmail.com>
2020-09-07 15:38:15 +0200

    Modul 13 - Kenneth

--- robocontroller/modules/modul_13.py
+++ robocontroller/modules/modul_13.py
@@ -1,6 +1,7 @@
-import navigation
+from navigation import follow_line, seek_line, follow_line_white
+
 # kør frem til hvid rammes
-while line_sensor.reflection() not <= WHITE - 5:
+while line_sensor.reflection() != WHITE - 5:
     robot.drive(500)
 
 # mål afstand til væg
@@ -9,14 +10,20 @@ Afstand = distance()
 # del afstand med to
 Afstand_stop = Afstand / 2
 
+
 # kør frem til afstands måler viser forrige svar
-while True
+move = True
+
+while move = True
   Distance_measured = distance()
    if Distance_measured == Afstand_stop:
         robot.stop()
+        move = False
     else:
-        follow_line  # Hvid
+        follow_line_hvid
 
 
 # stop
 # Afspil we are the champions
+ev3.speaker.play_file("music.wav")
+wait(5000)
