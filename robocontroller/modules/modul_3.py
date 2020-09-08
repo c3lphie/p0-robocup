a610337a440e8e6c7a8a7122fb423199aaa06869
Karachnor <70699974+Karachnor@users.noreply.github.com>
2020-09-08 10:21:54 +0200

    modul 3 + lidt på modul 4 -klo

--- robocontroller/modules/modul_3.py
+++ robocontroller/modules/modul_3.py
@@ -1,3 +1,8 @@
+import navigation
 #Drej 45 grader mod venstre
+robot.turn(45)
 #kør til grå streg rammes
+robot.straight(200)
 #Følg linje til makør
+navigation.seek_line("left")
+navigation.follow_line()
