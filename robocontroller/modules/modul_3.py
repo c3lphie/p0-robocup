20b1367a251ef68ed8343a7a5e6f07588a979dd8
Karachnor <70699974+Karachnor@users.noreply.github.com>
2020-09-11 12:38:01 +0200

    update

--- robocontroller/modules/modul_3.py
+++ robocontroller/modules/modul_3.py
@@ -4,12 +4,13 @@ robot = WallE()
 #drej 45 grader mod venstre
 robot.turn(45)
 
+
 #kør til grå streg rammes
-while robot.line_sensor.reflection() == GREY
+while robot.line_sensor.reflection() > robot.GREY:
     robot.drive(100)
-    if robot.line_sensor.reflection() > GREY
-    robot.brake()
+    if robot.line_sensor.reflection() > robot.GREY:
+    robot.stop()
 
 #følg linje til markør
-navigation.seek_line("left")
-navigation.follow_line()
+robot.seek_line("left")
+robot.follow_line()
